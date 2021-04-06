731b451d4e4c34a949dcd848a6eb60d158d213fb
develad <develadb@gmail.com>
2021-04-06 14:53:00 +0300

    :tada: Adding About info

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function About() {
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
+      <Wrapper style={{ textAlign: 'center' }}>
         <h1>This is a Demo site!</h1>
         <h2>
           This project design to practice the react-router-dom package
@@ -13,11 +14,19 @@ function About() {
           Use the NavBar to navigate on the site or press on the user picture
         </h2>
         <h1 style={{ marginTop: '8rem' }}>
-          created by: Elad Bar {new Date().getFullYear()} &copy;
+          created by:
+          <br />
+          Elad Bar {new Date().getFullYear()} &copy;
         </h1>
-      </div>
+      </Wrapper>
     </>
   );
 }
 
 export default About;
+
+const Wrapper = styled.div`
+  @media (max-width: 600px) {
+    padding: 0 20px;
+  }
+`;
