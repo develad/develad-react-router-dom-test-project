247933ccaf1f091840ac4fd3c3f2ab04fc7ba364
develad <develadb@gmail.com>
2021-04-06 14:47:03 +0300

    :tada: Adding About info

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <h1>This is a Demo site!</h1>
+        <h2>
+          This project design to practice the react-router-dom package
+          capabilities
+        </h2>
+        <h2>
+          Use the NavBar to navigate on the site or press on the user picture
+        </h2>
+        <h1 style={{ marginTop: '8rem' }}>
+          created by: Elad Bar {new Date().getFullYear()} &copy;
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default About;
